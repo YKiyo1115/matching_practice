1d3465968ce29771e401ace066bb74e162937c61
KentaTashiro <62204430+KentaTashiro@users.noreply.github.com>
2021-01-31 15:02:00 +0900

    プロジェクト ファイルを追加します。

---
+++ matching_practice.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.button import Label
+
+class TestApp(App):
+    def build(self):
+        pass
+        #return Label(text='Hello World')
+TestApp().run()
