5fd11afe5b99dc34f065076cb5afa38b791779c4
KentaTashiro <62204430+KentaTashiro@users.noreply.github.com>
2021-02-01 23:00:28 +0900

    Update matching_practice.py
    試しにコミット

--- matching_practice.py
+++ matching_practice.py
@@ -1,9 +1,9 @@
 from kivy.app import App
 from kivy.uix.button import Button
-from kivy.uix.button import Label
+from kivy.uix.label import Label
 
 class TestApp(App):
     def build(self):
         pass
-        #return Label(text='Hello World')
+        #return Button(text='Hello World')
 TestApp().run()
